1b47fa1757c673125fec46556d868f591e421d95
Christopher Miller <admin@mctherealm.net>
2016-11-21 23:00:35 -0500

    Improved JS style in liking to XO

--- js/main.js
+++ js/main.js
@@ -1,64 +1,71 @@
-const {clipboard} = require('electron')
+/* global PDFJS */
+
+const {clipboard} = require('electron');
+const path = require('path');
 
 function renderEditor(array) {
   $('#editingWindow').empty();
-  for (var row in array) {
-    var rowObj = $('#baseEditorRow').clone();
-    rowObj.attr('id', 'row-' + row);
-    rowObj.text(array[row]);
-    rowObj.removeAttr('hidden');
-    rowObj.appendTo('#editingWindow');
+  for (const row in array) {
+    if (array[row] !== null) {
+      const rowObj = $('#baseEditorRow').clone();
+      rowObj.attr('id', 'row-' + row);
+      rowObj.text(array[row]);
+      rowObj.removeAttr('hidden');
+      rowObj.appendTo('#editingWindow');
+    }
   }
 }
 
 $(document).ready(() => {
-  var fs = require('graceful-fs');
+  const fs = require('graceful-fs');
+
   PDFJS.workerSrc = 'bower_components/pdfjs-dist/build/pdf.worker.js';
 
-  var currentLineNum = 1;
-  var editorText = [];
+  let currentLineNum = 1;
+  const editorText = [];
   renderEditor(editorText);
 
   $('#run').click(() => {
-    var minifiedText = '';
-    for (var i in editorText) {
-      minifiedText += editorText[i]
+    let minifiedText = '';
+    for (const i in editorText) {
+      if (editorText[i] !== null) {
+        minifiedText += editorText[i];
+      }
     }
-    require("latex")([minifiedText]).pipe(fs.createWriteStream("test.pdf"));
-    var url = 'file://' + __dirname + '/test.pdf';
-    PDFJS.getDocument(url).then((pdf) => {
+    require('latex')([minifiedText]).pipe(fs.createWriteStream('test.pdf'));
+    const url = path.join('file://', __dirname, '/test.pdf');
+    PDFJS.getDocument(url).then(pdf => {
       //
       // Fetch the first page
       //
-      pdf.getPage(1).then((page) => {
-        var proportion = $('#viewingWindow').width() / page.getViewport(1).width;
-        var viewport = page.getViewport(proportion);
-        console.log(viewport);
-
+      pdf.getPage(1).then(page => {
+        const proportion = $('#viewingWindow').width() / page.getViewport(1).width;
+        const viewport = page.getViewport(proportion);
 
         //
         // Prepare canvas using PDF page dimensions
         //
-        var canvas = document.getElementById('pdfViewer');
-        var context = canvas.getContext('2d');
+        const canvas = document.getElementById('pdfViewer');
+        const context = canvas.getContext('2d');
         canvas.height = $('#viewingWindow').height();
         canvas.width = $('#viewingWindow').width();
 
         //
         // Render PDF page into canvas context
         //
-        var renderContext = {
+        const renderContext = {
           canvasContext: context,
-          viewport: viewport
+          viewport
         };
         page.render(renderContext);
       });
     });
   });
 
-  var functionKey = 0;
-  $(document).keydown((event) => {
-    switch(event.which) {
+  let functionKey = 0;
+  // TODO: Add key up event to resassign functionKey
+  $(document).keydown(event => {
+    switch (event.which) {
       // Control Key
       case 17:
         functionKey = 17;
@@ -69,7 +76,7 @@ $(document).ready(() => {
         break;
       // Backspace
       case 8:
-        if (editorText[currentLineNum] == "") {
+        if (editorText[currentLineNum] === '') {
           currentLineNum--;
         } else {
           editorText[currentLineNum] = editorText[currentLineNum].slice(0, -1);
@@ -77,21 +84,21 @@ $(document).ready(() => {
         }
         break;
       default:
-        if (editorText[currentLineNum] == null) {
-          editorText[currentLineNum] = "";
+        if (editorText[currentLineNum] === undefined) {
+          editorText[currentLineNum] = '';
         }
-        if (functionKey != 0) {
+        if (functionKey !== 0) {
           switch (event.which) {
-            case 86:
-              var pasteData = clipboard.readText().split('\n');
-              console.log(pasteData);
-              for (var i in pasteData) {
-                  if (editorText[currentLineNum + i] == null) {
-                    editorText[currentLineNum + i] = '';
-                  }
-                  editorText[currentLineNum + i] += pasteData[i];
+            case 86: {
+              const pasteData = clipboard.readText().split('\n');
+              for (const i in pasteData) {
+                if (editorText[currentLineNum + i] === undefined) {
+                  editorText[currentLineNum + i] = '';
+                }
+                editorText[currentLineNum + i] += pasteData[i];
               }
               break;
+            }
             default:
               editorText[currentLineNum] += event.key;
           }
